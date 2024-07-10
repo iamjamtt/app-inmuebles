7a5dfd1767df9db0b14257d6757f0cf212936ba9
Jamt Mendoza <43344540+iamjamtt@users.noreply.github.com>
2024-07-09 23:13:04 -0500

    feat: Actualizar botones de inicio de sesión y registro en el layout de usuario

--- resources/views/components/layouts/user.blade.php
+++ resources/views/components/layouts/user.blade.php
@@ -28,8 +28,8 @@
 
         <x-slot:actions>
             <x-button label="Inicio" icon="o-home" link="/" class="hidden btn-ghost lg:flex" responsive />
-            <x-button label="Ingresar" icon="o-arrow-top-right-on-square" class="btn-success" responsive />
-            <x-button label="Registrate" icon="o-user-plus" class="btn-outline" responsive />
+            <x-button label="Ingresar" icon="o-arrow-top-right-on-square" link="/login" class="btn-success" responsive />
+            <x-button label="Registrate" icon="o-user-plus" link="/" class="btn-outline" responsive />
         </x-slot:actions>
     </x-nav>
 
