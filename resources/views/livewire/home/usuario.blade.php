5d68800304e3d50d819bb6ff8408b6fda14b4629
Jamt Mendoza <43344540+iamjamtt@users.noreply.github.com>
2024-07-11 17:33:20 -0500

    feat: carga de pisos y habitaciones en el mostrar inmueble

--- resources/views/livewire/home/usuario.blade.php
+++ resources/views/livewire/home/usuario.blade.php
@@ -22,11 +22,15 @@ new #[Title('App Inmuebles')] #[Layout('components.layouts.user')] class extends
     public string $buttonModal = '';
     public string $actionModal = '';
 
-    public function cargar_inmueble(Inmueble $inmueble): void
+    public function cargar_inmueble($InmId): void
     {
         if (auth()->check()) {
             $usuario = auth()->user();
             if ($usuario->rol->RolNombre === 'Cliente') {
+                $inmueble = Inmueble::query()
+                    ->where('InmId', $InmId)
+                    ->with('pisos.habitaciones')
+                    ->first();
                 $this->inmueble = $inmueble;
                 $this->titleModal = 'Inmueble: ' . $inmueble->InmNombre;
                 $this->subtitleModal = 'Aquí puedes ver la información del inmueble.';
@@ -81,20 +85,20 @@ new #[Title('App Inmuebles')] #[Layout('components.layouts.user')] class extends
 
     {{-- mostrar card de inmuebles para el usuario --}}
     <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-        @foreach ($inmuebles as $inmueble)
-            <x-card title="{{ $inmueble->InmNombre }}" wire:key="{{ $inmueble->InmId }}">
-                {{ $inmueble->InmDescripcion }}
+        @foreach ($inmuebles as $item)
+            <x-card title="{{ $item->InmNombre }}" wire:key="{{ $item->InmId }}">
+                {{ $item->InmDescripcion }}
 
                 <x-slot:figure>
-                    <img src="{{ asset($inmueble->InmFoto) }}" class="object-cover w-full h-60" alt="{{ $inmueble->InmNombre }}">
+                    <img src="{{ asset($item->InmFoto) }}" class="object-cover w-full h-60" alt="{{ $item->InmNombre }}">
                 </x-slot:figure>
                 <x-slot:menu>
                     <span>
-                        S/. {{ calcularPrecioInmueble($inmueble->InmId) }}
+                        S/. {{ calcularPrecioInmueble($item->InmId) }}
                     </span>
                 </x-slot:menu>
                 <x-slot:actions>
-                    <x-button label="Ver inmueble" icon="o-eye" class="btn-primary" wire:click="cargar_inmueble({{ $inmueble->InmId }})" />
+                    <x-button label="Ver inmueble" icon="o-eye" class="btn-primary" wire:click="cargar_inmueble({{ $item->InmId }})" />
                 </x-slot:actions>
             </x-card>
         @endforeach
@@ -104,8 +108,41 @@ new #[Title('App Inmuebles')] #[Layout('components.layouts.user')] class extends
     </div>
 
     <!-- MODALS -->
-    <x-modal wire:model="modalInmueble" title="{{ $titleModal }}" subtitle="{{ $subtitleModal }}" box-class="w-11/12 max-w-5xl" separator>
+    <x-modal wire:model="modalInmueble" title="{{ $titleModal }}" subtitle="{{ $subtitleModal }}" box-class="w-11/12 max-w-6xl" separator>
         <x-form>
+            @if ($inmueble)
+                @foreach ($inmueble->pisos as $piso)
+                    <x-card
+                        title="{{ 'Piso N°'.$piso->PisInmNumeroPiso }}"
+                        class="border border-gray-200 rounded-none"
+                        separator
+                    >
+                        <div class="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+                            @foreach (
+                                $piso->habitaciones()
+                                    ->where('HabInmEstado', true)
+                                    ->where('HabInmOcupado', false)
+                                    ->get() as $habitacion
+                            )
+                                <div class="px-4 border border-gray-200">
+                                    <x-checkbox wire:model="item4">
+                                        <x-slot:label>
+                                            <x-card wire:key="{{ $habitacion->HabId }}">
+                                                <div class="text-sm font-bold">
+                                                    {{ $habitacion->HabInmNombre }}
+                                                </div>
+                                                <span>
+                                                    S/. {{ $habitacion->HabInmPrecio }}
+                                                </span>
+                                            </x-card>
+                                        </x-slot:label>
+                                    </x-checkbox>
+                                </div>
+                            @endforeach
+                        </div>
+                    </x-card>
+                @endforeach
+            @endif
             {{-- <x-input label="Nombre" required wire:model="nombre_habitacion" placeholder="Ingrese el nombre de la habitación" />
             <x-input label="Precio" required wire:model="precio_habitacion" money locale="es-PE" /> --}}
         </x-form>
